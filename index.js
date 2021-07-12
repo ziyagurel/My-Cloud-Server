030035317f1590547d4b5c747e363a8c5142dc89
ziyagurel <ziyagurel55@hotmail.com>
2021-07-12 10:54:09 +0300

    updated string

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ app.get('/', (req,res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = 'mongodb+srv://ziyagurel:ziyagurel55@cluster0.tfsmn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
