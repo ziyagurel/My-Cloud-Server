ed4cddcede203b8f556b1b0192b2a7772ca2bf85
ziyagurel <ziyagurel55@hotmail.com>
2021-07-12 10:23:28 +0300

    updated port

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', (req,res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(PORT, {useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
